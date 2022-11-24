688ddc8a7cef08ca230cb63859c4517033bfe3ac
Ankan <ankankundu2002@yahoo.com>
2022-11-24 21:57:39 +0530

    made the portfolio

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,13 +6,13 @@ const Contact = () => {
     <footer class="bg-[#001219] dark:bg-gray-800 pt-4 pb-8 xl:pt-8">
         <div class="pt-8 flex border-t border-gray-200 max-w-xs mx-auto items-center justify-between">
             <button class='hover:text-[#e9d8a6] text-[#94d2bd] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-            <a  href="https://github.com/Ankan8902" target="_blank" rel="noopener noreferrer"><FiGithub /></a>
+            <a  href="https://github.com/aaronvarma" target="_blank" rel="noopener noreferrer"><FiGithub /></a>
             </button>
             <button class='hover:text-[#e9d8a6] text-[#94d2bd] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>
-                <a  href="https://www.linkedin.com/in/ankan-kundu-084b51232/" type="a" target="_blank" rel="noopener noreferrer"><FiLinkedin/></a>
+                <a  href=" https://www.linkedin.com/in/anshu-aaron-varma-a96a48240/" type="a" target="_blank" rel="noopener noreferrer"><FiLinkedin/></a>
             </button>
             <button class='hover:text-[#e9d8a6] text-[#94d2bd] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-            <a  href="https://drive.google.com/file/d/1cuh2Bd9vDRRADNHKXub_FU13xsc7unAz/view?usp=sharing" target="_blank" rel="noopener noreferrer"><FiMeh /></a>
+            <a  href=" https://www.instagram.com/anshuaronvarma/" target="_blank" rel="noopener noreferrer"><FiMeh /></a>
             </button>
         </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,18 +6,18 @@ const Projects = () => {
         <h1 className='text-[#94d2bd] text-4xl'>Projects:</h1>
         <div>
           <ul className='list-disc'>
-            <li>
+            {/* <li>
               <a href='https://spotter1.netlify.app' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Spotter</a>
-            </li>
+            </li> */}
             <li>
               <a href='https://ankan8902.github.io/Loruki-website/' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Loruki-Website</a>
             </li>
             <li>
               <a href='https://ankan8902.github.io/Creative-Agency-Website/' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Creative-Agency Website</a>
             </li>
-            <li>
+            {/* <li>
               <a href='https://wokeye.netlify.app/login' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Wokeye</a>
-            </li>
+            </li> */}
           </ul>
         </div>
     </div>
